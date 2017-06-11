00b345bfcfb2c9ab87d0cf853d3962b15f714550
Raphael Oliveira <rapahaeru@gmail.com>
2017-06-11 18:10:09 -0300

    WIP - analise de url

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const server = express();
 
 server.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(3000, () => console.log('Server started! :)'));
+server.listen(3000, () => console.log('Server started!'));
