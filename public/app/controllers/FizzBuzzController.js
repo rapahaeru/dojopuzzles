eb1130b848ecc2e41502cb0240627b8697605285
Raphael Oliveira <rapahaeru@gmail.com>
2017-09-28 21:35:40 -0300

    Adiciona testes de unidade no dojo de fizzBuzz

--- public/app/controllers/FizzBuzzController.js
+++ public/app/controllers/FizzBuzzController.js
@@ -1,19 +1,19 @@
 class FizzBuzzController {
 	
 	constructor() {
-		this._numberOfLines = 100;
-		this._FizzBuzzModel = new FizzBuzz();
-		this._FizzBuzzView = new FizzBuzzView(document.querySelector('#display'));
+		this.numberOfLines = 100;
+		this.FizzBuzzModel = new FizzBuzz();
+		this.FizzBuzzView = new FizzBuzzView(document.querySelector('#display'));
 	}
 
 	loadData() {
 		
-		for (var x=0; x<=this._numberOfLines; x++) {
+		for (var x=0; x<=this.numberOfLines; x++) {
 			let byThree = x % 3,
 				byFive = x % 5,
-				analyzed = this._FizzBuzzModel.isDivisible(x, byThree, byFive) || x;
+				analyzed = this.FizzBuzzModel.isDivisible(x, byThree, byFive) || x;
 
-				this._FizzBuzzView.displayAppended(analyzed);
+				this.FizzBuzzView.displayAppended(analyzed);
 		}
 
 	}
